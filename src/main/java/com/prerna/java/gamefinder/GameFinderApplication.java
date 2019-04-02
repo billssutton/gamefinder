c7cab4b9fa7eca88746f42889e7f3241eb311b73
Bill <wsutto@gmail.com>
2019-04-01 21:01:52 -0500

    testing

---
+++ src/main/java/com/prerna/java/gamefinder/GameFinderApplication.java
@@ -0,0 +1,13 @@
+package com.prerna.java.gamefinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameFinderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GameFinderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/prerna/java/gamefinder/GameFinderApplicationTests.java
@@ -0,0 +1,16 @@
+package com.prerna.java.gamefinder;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GameFinderApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
